18340c41dd8768e5d698d57f75749f019f84aea7
nealwauters <wautersneal2@gmail.com>
2023-08-16 21:20:25 +0200

    deleted players api ftm to focus on seasons , also made a html page where the user can choose a season from the database

--- B.A.D/backend/app.js
+++ B.A.D/backend/app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { Client } = require('pg'); 
+const path = require('path');
+const { Client } = require('pg');
 const port = 3000;
-
-
 
 // PostgreSQL database configuration
 const db = new Client({
@@ -27,66 +26,31 @@ db.connect()
 // Use the cors middleware
 app.use(cors());
 
-// Middleware for logging requests
-app.use((req, res, next) => {
-    console.log(`[${new Date().toLocaleString()}] ${req.method} request to ${req.url}`);
-    next();
-});
+// Serve static files from the frontend folder (adjusting the path)
+app.use(express.static(path.join(__dirname, '../frontend')));
 
-
+// Serve the index.html file from the frontend folder (adjusting the path)
 app.get('/', (req, res) => {
-    res.send('Welcome to Premier League API');
+    res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
 });
 
-// Endpoint to retrieve all players from the PostgreSQL database
-app.get('/api/players', async (req, res) => {
-    try {
-        // Retrieve players from the PostgreSQL database
-        const query = 'SELECT * FROM "Players"';
-        const result = await db.query(query);
-        const players = result.rows; 
-        res.json(players);
-    } catch (err) {
-        console.error('Error fetching players from PostgreSQL:', err);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-});
 
+// Endpoint to retrieve all seasons from the PostgreSQL database
 app.get('/api/seasons', async (req, res) => {
-  try {
-      // Retrieve seasons from the PostgreSQL database
-      const query = 'SELECT * FROM "Seasons"';
-      const result = await db.query(query);
-      const seasons = result.rows;
-      res.json(seasons);
-  } catch (err) {
-      console.error('Error fetching seasons from PostgreSQL:', err);
-      res.status(500).json({ message: 'Internal server error' });
-  }
-});
-
-
-
-
-// Endpoint to retrieve a specific player by ID from the PostgreSQL database
-app.get('/api/players/:id', async (req, res) => {
-    const playerId = parseInt(req.params.id);
     try {
-        // Retrieve player by ID from the PostgreSQL database
-        const query = 'SELECT * FROM "Players" WHERE id = $1';
-        const result = await db.query(query, [playerId]);
-
-        if (result.rows.length === 1) {
-            const player = result.rows[0];
-            res.json(player);
-        } else {
-            res.status(404).json({ message: 'Player not found' });
-        }
+        // Retrieve seasons from the PostgreSQL database
+        const query = 'SELECT * FROM "Seasons"';
+        const result = await db.query(query);
+        const seasons = result.rows;
+        res.json(seasons);
+        console.log(seasons);
     } catch (err) {
-        console.error('Error fetching player from PostgreSQL:', err);
+        console.error('Error fetching seasons from PostgreSQL:', err);
         res.status(500).json({ message: 'Internal server error' });
     }
 });
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
