8f721832f39e90af0284194a8307095778b04356
nealwauters <wautersneal2@gmail.com>
2023-08-16 21:17:40 +0200

    cleaned it a but up

--- B.A.D/backend/APIdb.js
+++ B.A.D/backend/APIdb.js
@@ -1,4 +1,5 @@
 const { Client } = require('pg');
+const fetch = require('node-fetch');
 
 const db = new Client({
     user: 'postgres',
@@ -12,16 +13,14 @@ const mockSeasons = [
     {
         id: 'sr:season:1',
         name: 'Mock Season 1',
-        
     },
     {
         id: 'sr:season:2',
         name: 'Mock Season 2',
-    
     },
 ];
 
-async function insertMockData() {
+async function insertDataFromApiAndMockData() {
     const newDbClient = new Client({
         user: 'postgres',
         password: 'your_postgres_password',
@@ -33,9 +32,23 @@ async function insertMockData() {
     try {
         await newDbClient.connect();
 
-        for (const season of mockSeasons) {
-            const { id, name} = season;
+        // Fetch data from the external API
+        const apiUrl = "http://api.sportradar.us/darts/trial/v2/en/seasons.json?api_key=b3ah5dwxbswnjzguuhu5q5uj";
+        const response = await fetch(apiUrl);
+        const apiData = await response.json();
+        const externalApiSeasons = apiData.seasons;
 
+        // Insert data from the external API (id and name only)
+        for (const season of externalApiSeasons) {
+            const { id, name } = season;
+            const query = 'INSERT INTO "Seasons" (id, name) VALUES ($1, $2)';
+            await newDbClient.query(query, [id, name]);
+            console.log('External API data inserted into "Seasons" table:', { id, name });
+        }
+
+        // Insert mock data
+        for (const season of mockSeasons) {
+            const { id, name } = season;
             const query = 'INSERT INTO "Seasons" (id, name) VALUES ($1, $2)';
             await newDbClient.query(query, [id, name]);
             console.log('Mock data inserted into "Seasons" table:', season);
@@ -50,7 +63,7 @@ async function insertMockData() {
 db.connect()
     .then(() => {
         console.log('Connected to PostgreSQL database');
-        insertMockData();
+        insertDataFromApiAndMockData();
     })
     .catch(err => {
         console.error('Error connecting to PostgreSQL database:', err);
