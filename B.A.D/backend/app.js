0d9b54f285d69be035b44b6c27337fc639353cf3
nealwauters <wautersneal2@gmail.com>
2023-08-16 05:57:24 +0200

    made connection to the database and edited the endpoints for it

--- B.A.D/backend/app.js
+++ B.A.D/backend/app.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const app = express();
-const cors = require('cors'); 
+const cors = require('cors');
+const { Client } = require('pg'); // Import PostgreSQL module
 const port = 3000;
 
-// Mock database of Premier League teams
-const mockDatabase = [
-    { id: 1, name: 'MVG', city: '96' },
-    { id: 2, name: 'GP ', city: '101' },
-    { id: 3, name: 'DVB', city: '91' },
-    // ... add more teams here ...
-];
+// PostgreSQL database configuration
+const db = new Client({
+    user: 'postgres',
+    password: 'your_postgres_password',
+    host: 'localhost',
+    port: 5431, // Default PostgreSQL port
+    database: 'firstDatabase'
+});
+
+// Connect to the PostgreSQL database
+db.connect()
+    .then(() => {
+        console.log('Connected to PostgreSQL database');
+    })
+    .catch(err => {
+        console.error('Error connecting to PostgreSQL database:', err);
+    });
+
 // Use the cors middleware
 app.use(cors());
-
 
 // Middleware for logging requests
 app.use((req, res, next) => {
@@ -24,21 +35,39 @@ app.get('/', (req, res) => {
     res.send('Welcome to Premier League API');
 });
 
-app.get('/api/teams', (req, res) => {
-    res.json(mockDatabase);
-});
-
-app.get('/api/teams/:id', (req, res) => {
-    const teamId = parseInt(req.params.id);
-    const team = mockDatabase.find(team => team.id === teamId);
-    
-    if (team) {
-        res.json(team);
-    } else {
-        res.status(404).json({ message: 'Team not found' });
+// Endpoint to retrieve all players from the PostgreSQL database
+app.get('/api/players', async (req, res) => {
+    try {
+        // Retrieve players from the PostgreSQL database
+        const query = 'SELECT * FROM "Players"'; // Use double quotes for case-sensitive table name
+        const result = await db.query(query);
+        const players = result.rows; // Extract players from the query result
+        res.json(players);
+    } catch (err) {
+        console.error('Error fetching players from PostgreSQL:', err);
+        res.status(500).json({ message: 'Internal server error' });
     }
 });
 
+// Endpoint to retrieve a specific player by ID from the PostgreSQL database
+app.get('/api/players/:id', async (req, res) => {
+    const playerId = parseInt(req.params.id);
+    try {
+        // Retrieve player by ID from the PostgreSQL database
+        const query = 'SELECT * FROM "Players" WHERE id = $1'; // Use double quotes for case-sensitive table name
+        const result = await db.query(query, [playerId]);
+
+        if (result.rows.length === 1) {
+            const player = result.rows[0]; // Extract the player from the query result
+            res.json(player);
+        } else {
+            res.status(404).json({ message: 'Player not found' });
+        }
+    } catch (err) {
+        console.error('Error fetching player from PostgreSQL:', err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
--- B.A.D/frontend/app.js
+++ B.A.D/frontend/app.js
@@ -1,23 +1,23 @@
-const teamList = document.getElementById("teamList");
+const playerList = document.getElementById("playerList");
 
 console.log("Fetching data from backend..."); // Added console log
 
-fetch("http://localhost:3000/api/teams") // Replace with your backend's address
+fetch("http://localhost:3000/api/players") // Replace with your backend's address
     .then(response => {
         console.log("Response status:", response.status); // Added console log
         return response.json();
     })
     .then(data => {
         console.log("Data fetched successfully:", data); // Added console log
-        data.forEach(team => {
+        data.forEach(player => {
             const listItem = document.createElement("li");
-            listItem.textContent = `${team.name} - ${team.city}`;
-            teamList.appendChild(listItem);
+            listItem.textContent = `${player.name} - ${player.country}`;
+            playerList.appendChild(listItem);
         });
     })
     .catch(error => {
         console.error("Error fetching data:", error);
         const errorMessage = document.createElement("li");
         errorMessage.textContent = "An error occurred while fetching data.";
-        teamList.appendChild(errorMessage);
+        playerList.appendChild(errorMessage);
     });
