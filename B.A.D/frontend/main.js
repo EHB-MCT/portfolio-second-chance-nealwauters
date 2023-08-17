5c18870fee7e56119895784fa18b8eaa4c61742e
nealwauters <wautersneal2@gmail.com>
2023-08-17 21:12:46 +0200

    made sure the user can choose 2 players and their stats are shown seperatly, to eventually compare the graphicly

--- B.A.D/frontend/main.js
+++ B.A.D/frontend/main.js
@@ -1,6 +1,7 @@
-
 const seasonDropdown = document.getElementById('seasonDropdown');
 const playerDropdown = document.getElementById('playerDropdown');
+const playerDropdown2 = document.getElementById('playerDropdown2');
+
 const averageDataDisplay = document.getElementById('averageDataDisplay');
 const statisticsDisplay = document.getElementById('statistics');
 
@@ -12,13 +13,15 @@ seasonDropdown.addEventListener('change', handleSeasonSelection);
 
 // Event listener for player dropdown change
 playerDropdown.addEventListener('change', handlePlayerSelection);
+playerDropdown2.addEventListener('change', handlePlayerSelection); 
 
 // Function to handle player selection
 function handlePlayerSelection() {
     const selectedPlayerId = playerDropdown.value;
+    const selectedPlayerId2 = playerDropdown2.value;
     const selectedSeasonId = seasonDropdown.value;
 
-    if (selectedPlayerId && selectedSeasonId) {
+    if (selectedPlayerId && selectedPlayerId2 && selectedSeasonId) {
         fetchPlayerStatistics(selectedSeasonId, selectedPlayerId);
     } else {
         clearStatistics();
@@ -61,11 +64,13 @@ function fetchPlayers(seasonId) {
         .then(response => response.json())
         .then(players => {
             clearPlayerDropdown();
+            clearPlayerDropdown2();
             players.forEach(player => {
                 const option = document.createElement('option');
                 option.value = player.id;
                 option.textContent = player.name;
                 playerDropdown.appendChild(option);
+                playerDropdown2.appendChild(option.cloneNode(true));
             });
         })
         .catch(error => {
@@ -76,27 +81,31 @@ function fetchPlayers(seasonId) {
 
 // Function to fetch statistics for the selected player
 function fetchPlayerStatistics(seasonId, playerId) {
+    const targetDiv = playerId === playerDropdown.value ? 'statisticsPlayer1' : 'statisticsPlayer2';
+
     fetch(`/api/seasons/${seasonId}/players/${playerId}/statistics`)
         .then(response => response.json())
         .then(statistics => {
+            console.log('Received statistics:', statistics);
             // Display player statistics separately in the statisticsDisplay element
             const formattedStatistics = `
-                <p><button class="stat-label" onclick="toggleStat(this)"><span class="stat-name">Average 3 Darts:</span></button> <span class="stat-value hidden">${statistics.statistics.average_3_darts}</span></p>
-                <p><button class="stat-label" onclick="toggleStat(this)"><span class="stat-name">Checkout Percentage:</span></button> <span class="stat-value hidden">${statistics.statistics.checkout_percentage}%</span></p>
-                <p><button class="stat-label" onclick="toggleStat(this)"><span class="stat-name">Checkouts:</span></button> <span class="stat-value hidden">${statistics.statistics.checkouts}</span></p>
-                <p><button class="stat-label" onclick="toggleStat(this)"><span class="stat-name">Checkouts (100s+):</span></button> <span class="stat-value hidden">${statistics.statistics.checkouts_100s_plus}</span></p>
-                <p><button class="stat-label" onclick="toggleStat(this)"><span class="stat-name">Highest Checkout:</span></button> <span class="stat-value hidden">${statistics.statistics.highest_checkout}</span></p>
-                <p><button class="stat-label" onclick="toggleStat(this)"><span class="stat-name">Scores (100s+):</span></button> <span class="stat-value hidden">${statistics.statistics.scores_100s_plus}</span></p>
-                <p><button class="stat-label" onclick="toggleStat(this)"><span class="stat-name">Scores (140s+):</span></button> <span class="stat-value hidden">${statistics.statistics.scores_140s_plus}</span></p>
-                <p><button class="stat-label" onclick="toggleStat(this)"><span class="stat-name">Scores (180s):</span></button> <span class="stat-value hidden">${statistics.statistics.scores_180s}</span></p>
+                <p><span class="stat-label" onclick="toggleStat(this)">Average 3 Darts:</span> <span class="stat-value">${statistics.statistics.average_3_darts}</span></p>
+                <p><span class="stat-label" onclick="toggleStat(this)">Checkout Percentage:</span> <span class="stat-value">${statistics.statistics.checkout_percentage}%</span></p>
+                <p><span class="stat-label" onclick="toggleStat(this)">Checkouts:</span> <span class="stat-value">${statistics.statistics.checkouts}</span></p>
+                <p><span class="stat-label" onclick="toggleStat(this)">Checkouts (100s+):</span> <span class="stat-value">${statistics.statistics.checkouts_100s_plus}</span></p>
+                <p><span class="stat-label" onclick="toggleStat(this)">Highest Checkout:</span> <span class="stat-value">${statistics.statistics.highest_checkout}</span></p>
+                <p><span class="stat-label" onclick="toggleStat(this)">Scores (100s+):</span> <span class="stat-value">${statistics.statistics.scores_100s_plus}</span></p>
+                <p><span class="stat-label" onclick="toggleStat(this)">Scores (140s+):</span> <span class="stat-value">${statistics.statistics.scores_140s_plus}</span></p>
+                <p><span class="stat-label" onclick="toggleStat(this)">Scores (180s):</span> <span class="stat-value">${statistics.statistics.scores_180s}</span></p>
             `;
-            statisticsDisplay.innerHTML = formattedStatistics;
+            const targetSectionId = playerId === playerDropdown.value ? 'stats-section-player1' : 'stats-section-player2';
+            const targetDiv = document.getElementById(targetSectionId);
+            targetDiv.innerHTML = formattedStatistics;
         })
         .catch(error => {
             console.error('Error fetching player statistics:', error);
         });
 }
-
 
 // Function to toggle the visibility of the stat value
 function toggleStat(statLabel) {
@@ -104,51 +113,14 @@ function toggleStat(statLabel) {
     statValue.classList.toggle('hidden');
 }
 
-let activeStatButton = null; // To keep track of the active stat button
-
-// Function to toggle stat visibility
-function toggleStat(button) {
-    if (activeStatButton === button) {
-        // If the clicked button is already active, deactivate it
-        deactivateStatButton();
-    } else {
-        // Deactivate the previously active button (if any)
-        deactivateStatButton();
-        // Activate the clicked button and show its value
-        activeStatButton = button;
-        button.classList.add('active');
-        showStatValue(button);
-    }
-}
-
-// Function to deactivate the active stat button and hide its value
-function deactivateStatButton() {
-    if (activeStatButton) {
-        activeStatButton.classList.remove('active');
-        hideStatValue(activeStatButton);
-        activeStatButton = null;
-    }
-}
-
-// Function to show the value of the clicked stat button
-function showStatValue(button) {
-    const statValue = button.nextElementSibling;
-    statValue.classList.remove('hidden');
-}
-
-// Function to hide the value of the stat button
-function hideStatValue(button) {
-    const statValue = button.nextElementSibling;
-    statValue.classList.add('hidden');
-}
-
-
-
-
 
 // Function to clear player dropdown
 function clearPlayerDropdown() {
     playerDropdown.innerHTML = '<option value="">Select a player...</option>';
+}
+// Function to clear the second player dropdown
+function clearPlayerDropdown2() {
+    playerDropdown2.innerHTML = '<option value="">Select another player...</option>';
 }
 
 // Function to clear average data display
@@ -158,15 +130,25 @@ function clearAverageData() {
 
 // Function to clear statistics display
 function clearStatistics() {
-    statisticsDisplay.textContent = '';
+    document.getElementById('statisticsPlayer1').innerHTML = '';
+    document.getElementById('statisticsPlayer2').innerHTML = '';
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    // Hide all stat values when the page loads
-    const statValues = document.querySelectorAll('.stat-value');
-    statValues.forEach(statValue => {
-        statValue.classList.add('hidden');
-    });
+playerDropdown.addEventListener('change', function() {
+    const selectedPlayerId = playerDropdown.value;
+    if (selectedPlayerId && seasonDropdown.value) {
+        fetchPlayerStatistics(seasonDropdown.value, selectedPlayerId);
+    } else {
+        clearStatistics();
+    }
 });
 
+playerDropdown2.addEventListener('change', function() {
+    const selectedPlayerId2 = playerDropdown2.value;
+    if (selectedPlayerId2 && seasonDropdown.value) {
+        fetchPlayerStatistics(seasonDropdown.value, selectedPlayerId2);
+    } else {
+        clearStatistics();
+    }
+});
 
