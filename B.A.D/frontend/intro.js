5bac20bdc2deb68e06bfd2444d165a08a2bbab63
nealwauters <wautersneal2@gmail.com>
2023-08-18 05:53:51 +0200

    added a dartboard that reacts on the user scroll

--- B.A.D/frontend/intro.js
+++ B.A.D/frontend/intro.js
@@ -1,23 +1,65 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
 
 // Wait for the DOM to load before initializing the scene
 window.addEventListener('DOMContentLoaded', () => {
+    const sceneContainer = document.querySelector('.intro-scene');
     const canvas = document.getElementById('threeCanvas');
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer({ canvas });
+    
+    // Adjust the field of view for more detail
+    const camera = new THREE.PerspectiveCamera(45, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    cube.position.set(2, 0, 0); // Adjust the position to move the cube more to the right
-    scene.add(cube);
+    // Add ambient light to illuminate the scene
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
 
-    camera.position.z = 5;
+    // Add directional light to cast shadows
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(10, 10, 10); // Adjust the light position
+    scene.add(directionalLight);
 
-    renderer.setSize(canvas.clientWidth, canvas.clientHeight);
-    camera.aspect = canvas.clientWidth / canvas.clientHeight;
-    camera.updateProjectionMatrix();
+    const loader = new GLTFLoader();
+    let model;
 
-    renderer.render(scene, camera);
+    // Load the GLB model
+    loader.load('winmau_blade_5_dart_board.glb', (gltf) => {
+        model = gltf.scene;
+        model.scale.set(1, 1, 1); // Adjust the scale factors
+
+        scene.add(model);
+
+        // Position the model
+        model.position.set(160, 150, -190); 
+        model.rotation.y = THREE.MathUtils.degToRad(0);
+        model.rotation.x = THREE.MathUtils.degToRad(0);
+
+        // Camera position for a better view
+        camera.position.set(0, 0, 5);
+        camera.lookAt(0, 0, 0);
+
+        // Render the scene
+        renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.render(scene, camera);
+    });
+
+    // Add scroll event listener
+    let prevScrollPos = window.scrollY;
+    window.addEventListener('scroll', () => {
+        if (model) {
+            // Calculate rotation based on scroll position
+            const rotationSpeed = 0.005;
+            const scrollPos = window.scrollY;
+            const rotationAmount = (scrollPos - prevScrollPos) * rotationSpeed;
+            prevScrollPos = scrollPos;
+            
+            // Update the rotation of the model
+            model.rotation.y -= rotationAmount;
+            
+            // Render the updated scene
+            renderer.render(scene, camera);
+        }
+    });
 });
